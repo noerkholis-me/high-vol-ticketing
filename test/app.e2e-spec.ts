0a0c290f52bf9464f712816cb934f27c7ad8a21a
Nurkholis <noerkholis.madjid@gmail.com>
2026-01-05 20:14:37 +0700

    fix: app.e2e.spec testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,14 +3,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
-import { RedisService } from '@liaoliaots/nestjs-redis';
-import { PrismaService } from '../src/prisma/prisma.service';
 import request from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
-  let prisma: PrismaService;
-  let redis: RedisService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -18,18 +14,9 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    prisma = moduleFixture.get<PrismaService>(PrismaService);
-    redis = moduleFixture.get<RedisService>(RedisService); // If using decorator
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  afterEach(async () => {
-    await prisma.$disconnect(); // Close Prisma
-    const redisService = redis.getOrThrow(); // Close Redis
-    await redisService.quit();
-    await app.close(); // Close Nest app
   });
 
   it('should return Hello World!', async () => {
