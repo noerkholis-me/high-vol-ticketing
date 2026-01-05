d86a3ef10dd4c8a0672531eb6709e3d914e405c3
Nurkholis <noerkholis.madjid@gmail.com>
2026-01-05 20:07:30 +0700

    feat: Set up initial project structure with CI/CD, E2E tests, and basic app module.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
-import { RbacGuard } from './modules/rbac/guards/rbac.guard';
-import { Permissions } from './modules/rbac/decorators/permission.decorator';
 
 @Controller()
 @ApiTags('App')
 export class AppController {
   @Get()
-  @UseGuards(JwtAuthGuard, RbacGuard)
-  @Permissions('event:create')
   @ApiResponse({})
   getHello(): string {
     return 'Hello World!';
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,16 @@
+import 'suppress-experimental-warnings';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { RedisService } from '@liaoliaots/nestjs-redis';
+import { PrismaService } from '../src/prisma/prisma.service';
+import request from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let prisma: PrismaService;
+  let redis: RedisService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,7 +18,18 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = moduleFixture.get<PrismaService>(PrismaService);
+    redis = moduleFixture.get<RedisService>(RedisService); // If using decorator
+
+    app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterEach(async () => {
+    await prisma.$disconnect(); // Close Prisma
+    const redisService = redis.getOrThrow(); // Close Redis
+    await redisService.quit();
+    await app.close(); // Close Nest app
   });
 
   it('should return Hello World!', async () => {
