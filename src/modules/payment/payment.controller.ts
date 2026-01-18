b66a5a8a7d5ea5a5072f2c4b610c0bb1e67940b3
Nurkholis <noerkholis.madjid@gmail.com>
2026-01-18 19:18:28 +0700

    🛂 feat(auth): add guard and permissions at booking confirm endpoint

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { SuccessMessage } from '../../common/decorators/success-message.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RbacGuard } from '../rbac/guards/rbac.guard';
+import { Permissions } from '../rbac/decorators/permission.decorator';
 
 @ApiTags('Payment')
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
-  @Post(':bookingId/confirm')
   @ApiOkResponse()
   @SuccessMessage('Success confirm payment')
+  @UseGuards(JwtAuthGuard, RbacGuard)
+  @Permissions('payment:confirm')
+  @Post(':bookingId/confirm')
   create(@Param('bookingId') bookingId: string) {
     return this.paymentService.confirmPayment(bookingId);
   }
