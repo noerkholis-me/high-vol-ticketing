49b63f8d7434184317f7d8cdaf70b6556bf0f057
Nurkholis <noerkholis.madjid@gmail.com>
2026-01-19 23:12:52 +0700

    ✨ feat(config): add generated prisma folder to be ignore at jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     '^(\\.{1,2}\\/(?:internal\\/.*|enums))\\.js$': '$1.ts',
     '^uuid$': '<rootDir>/../test/mocks/uuid.ts',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverageFrom: ['**/*.(t|j)s', '!**/node_modules/**', '!generated/**'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
   clearMocks: true,
